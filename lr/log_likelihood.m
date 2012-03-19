5f704d50e7a19720344e3ed94fb8f6f49fd87f2b
Frank Wood <fwood@stat.columbia.edu>
2012-03-19 11:43:25 -0400

    Removed extra copies of hw3 autograding software

--- lr/log_likelihood.m
+++
@@ -1,8 +0,0 @@
-function ll = log_likelihood(X,Y,m,beta)
-% This function evaluates an approximation of the joint log likelihood.
-% The estimate uses the posterior mean of the weights.  This quantity
-% should increase as the algorithm progresses.
-
-lik = normpdf(Y - X*m,0,sqrt(1/beta));
-ll = sum(log(lik));
-
